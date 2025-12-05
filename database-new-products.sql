8a9057ef7a1b1b8cc62d23ef9e3e63b64cd1394e
viceculjak <viceculjak@users.noreply.github.com>
2025-12-05 04:28:22 +0100

    Fix NULL CAS values in product import SQL
    - Changed NULL to 'N/A' for 3 products without standard CAS numbers
    - HGH Fragment 176-191, GHRP-6, SLU-PP-332 now have 'N/A' as CAS
    - Fixes NOT NULL constraint violation during Supabase import

--- database-new-products.sql
+++ database-new-products.sql
@@ -11,11 +11,11 @@ INSERT INTO products (name, cas, smiles, mw, purity_options, image_url) VALUES
   ('BPC-157 + TB-500 Mix', 'N/A Mixture', NULL, 'N/A Mixture', ARRAY['≥95%', '≥98%', '≥99%'], '/structures/VC-007.svg'),
   ('Epithalon', '64082-79-7', NULL, '390.35 g/mol', ARRAY['≥95%', '≥98%', '≥99%'], '/structures/VC-008.svg'),
   ('PT-141', '189691-06-3', NULL, '1025.2 g/mol', ARRAY['≥95%', '≥98%', '≥99%'], '/structures/VC-009.svg'),
-  ('HGH Fragment 176-191', NULL, NULL, '1859.1 g/mol', ARRAY['≥95%', '≥98%', '≥99%'], '/structures/VC-010.svg'),
+  ('HGH Fragment 176-191', 'N/A', NULL, '1859.1 g/mol', ARRAY['≥95%', '≥98%', '≥99%'], '/structures/VC-010.svg'),
   ('Melanotan 2', '121062-08-6', NULL, '1024.2 g/mol', ARRAY['≥95%', '≥98%', '≥99%'], '/structures/VC-011.svg'),
   ('CJC-1295 DAC', '863288-34-0', NULL, '3649.1 g/mol', ARRAY['≥95%', '≥98%', '≥99%'], '/structures/VC-012.svg'),
   ('GHRP-2', '158861-67-7', NULL, '818.0 g/mol', ARRAY['≥95%', '≥98%', '≥99%'], '/structures/VC-013.svg'),
-  ('GHRP-6', NULL, NULL, '873.0 g/mol', ARRAY['≥95%', '≥98%', '≥99%'], '/structures/VC-014.svg'),
+  ('GHRP-6', 'N/A', NULL, '873.0 g/mol', ARRAY['≥95%', '≥98%', '≥99%'], '/structures/VC-014.svg'),
   ('Selank', '129954-34-3', NULL, '751.9 g/mol', ARRAY['≥95%', '≥98%', '≥99%'], '/structures/VC-015.svg'),
   ('Semax', '80714-61-0', NULL, '813.9 g/mol', ARRAY['≥95%', '≥98%', '≥99%'], '/structures/VC-016.svg'),
   ('MOTS-C', '1627580-64-6', NULL, '2174.6 g/mol', ARRAY['≥95%', '≥98%', '≥99%'], '/structures/VC-017.svg'),
@@ -31,7 +31,7 @@ INSERT INTO products (name, cas, smiles, mw, purity_options, image_url) VALUES
   ('Lipo NAD+', '53-84-9', NULL, '663.4 g/mol', ARRAY['≥95%', '≥98%', '≥99%'], '/structures/VC-027.svg'),
   ('NAD+ Capsules', '53-84-9', NULL, '663.4 g/mol', ARRAY['≥95%', '≥98%', '≥99%'], '/structures/VC-028.svg'),
   ('Bluemethyl', '13422-55-4', NULL, '1344.4 g/mol', ARRAY['≥95%', '≥98%', '≥99%'], '/structures/VC-029.svg'),
-  ('SLU-PP-332', NULL, NULL, '290.3 g/mol', ARRAY['≥95%', '≥98%', '≥99%'], '/structures/VC-030.svg'),
+  ('SLU-PP-332', 'N/A', NULL, '290.3 g/mol', ARRAY['≥95%', '≥98%', '≥99%'], '/structures/VC-030.svg'),
   ('TUDCA', '14605-22-2', NULL, '499.7 g/mol', ARRAY['≥95%', '≥98%', '≥99%'], '/structures/VC-031.svg'),
   ('Bacteriostatic Water', '7732-18-5', 'O', '18.015 g/mol', ARRAY['≥95%', '≥98%', '≥99%'], '/structures/VC-032.svg');
 
